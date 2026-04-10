f2f37b5638088dbe3031d2cd7cd9161154a9251e
oLfat0 <otaviogabrielso135@outlook.com>
2026-04-10 13:54:24 -0400

    add: lista5 - Arvores AVL [NÃO FINALIZADO]

---
+++ lista5_arvores_avl.c
@@ -0,0 +1,83 @@
+//NÃO FINALIZADO
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define M 25
+
+typedef struct no{
+    struct no *esq, *dir;
+    char info;
+} No;
+
+No* novo_no(char info);
+void imprima_estrutura(No* raiz, int nivel);
+void padding(char ch, int n);
+
+No* rotacao_direita(No* p);
+
+int main(){
+    No* raiz = novo_no('g');
+
+    //Descendentes de 'g'
+    raiz->esq = novo_no('e');
+    raiz->dir = novo_no('j');
+
+    //Descendentes de 'e'
+    raiz->esq->esq = novo_no('c');
+    raiz->esq->dir = novo_no('f');
+
+    raiz->esq->esq->esq = novo_no('b');
+    raiz->esq->esq->dir = novo_no('d');
+
+    raiz->esq->esq->esq->esq = novo_no('a');
+
+    //Descendentes de 'j'
+    raiz->dir->dir = novo_no('k');
+    printf("Arvore OG:\n");
+    imprima_estrutura(raiz, 0);
+
+    printf("Aplicando Rotacao-Direita (Left-Left)\n");
+    raiz->esq = rotacao_direita(raiz->esq);     //Nó 'e'
+
+    imprima_estrutura(raiz, 0);
+
+    return 0;
+}
+
+No* novo_no(char info){
+    No* novo = (No*) malloc(sizeof(No));
+    novo->info = info;
+    novo->esq = NULL;
+    novo->dir = NULL;
+
+    return novo;
+}
+
+void padding(char ch, int n){
+    for(int i=0; i<n; i++)
+        putchar(ch);
+}
+
+void imprima_estrutura(No* raiz, int nivel){
+    if(raiz == NULL){
+        padding('\t', nivel);
+        puts("~");
+    }else{
+        imprima_estrutura(raiz->dir, nivel+1);
+        padding('\t', nivel);
+        printf("%c \n", raiz->info);
+        imprima_estrutura(raiz->esq, nivel+1);
+    }
+}
+
+No* rotacao_direita(No* p){
+    //NÃO FINALIZADO
+    /* Left-Left */
+    No* pt = (No*) malloc(sizeof(No));
+    pt = p->esq;
+
+    p->esq = pt->dir;
+    pt->dir = p;
+    return pt;
+}
