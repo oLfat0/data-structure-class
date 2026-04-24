3b761e1bda5e76ac532cfb0ba233e813967aa1a4
oLfat0 <otaviogabrielso135@outlook.com>
2026-04-24 09:35:31 -0400

    add: switch com divisões binárias

--- atividades_ead/rascunhos/calc_mod.c
+++ atividades_ead/rascunhos/calc_mod.c
@@ -10,14 +10,42 @@ int hash_aux1(int k);
 int hash_aux2(int k);
 int hash_duplo(int k, int i);
 
+int divi_binaria(int k);
 
 int main(){
-    int x = 20;
-    int a = 1000, b = 2;
+    int x;
+    int h;
+    int a;
 
-    printf("Divisao Inteira: %d // %d = %d\n", a, b, a/b);
-    printf("MOD Normal: %d mod %d = %d\n", a, b, a%b);
-    printf("Hash Duplo de %d: Posi = %d\n", x, hash_duplo(x, 1));
+    // printf("Divisao Inteira: %d // %d = %d\n", a, b, a/b);
+    // printf("MOD Normal: %d mod %d = %d\n", a, b, a%b);
+    printf("O que fazer?\n");
+    printf("1: Hash Duplo | 2: Divisao Binaria\n");
+    scanf("%d", &x);
+
+    switch(x){
+        case 1:
+            printf("Hash Duplo:\n");
+            scanf("%d", &h);
+            printf("Hash Duplo de %d: Posi = %d\n", h, hash_duplo(h, 0));
+            break;
+        case 2:
+            while(1){
+                printf("Divisoes Binarias:\n");
+                scanf("%d", &a);
+                if(a!=0){
+                    printf("Inteiro | Resto\n");
+                    divi_binaria(a);
+                }
+                else{
+                    break;
+                }
+            }   
+            
+        default:
+            printf("Opcao invalida :(\n");
+    }
+    
     return 0;
 }
 
@@ -31,4 +59,12 @@ int hash_aux2(int k){
 
 int hash_duplo(int k, int i){
     return (hash_aux1(k) + i*hash_aux2(k)) % M;
+}
+
+int divi_binaria(int k){
+    if(k==0) return 0;
+
+    printf("%5d   ", k);
+    printf("%5d\n", k%2);
+    divi_binaria(k/2);
 }
