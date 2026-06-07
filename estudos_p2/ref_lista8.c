d17e71ae5160ce9e4226bf3b2a710d3cac39e178
oLfat0 <otaviogabrielso135@outlook.com>
2026-06-07 03:22:26 -0400

    add: função de busca não testada (questão 5)

--- estudos_p2/ref_lista8.c
+++ estudos_p2/ref_lista8.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define M 3
-
+typedef enum info{
+    TERMINAL,
+    NAO_TERMINAL
+} Info;
 typedef struct no{
     struct no* filhos[M];
+    Info info;
 }No;
+
 
 char alfabeto[M] = {'e', 'r', 's'};
 
@@ -15,7 +21,10 @@ int de_digi_pra_posi(char c);
 char de_posi_pra_digi(int p);
 
     /* Questão 3 */
-void percorre(No* pt, int p);
+void percorre(No* pt);
+
+    /* Questão 5 */
+No* busca_dig(char* x, No* pt, int l, int a);
 
 int main(){
     No* raiz = novo_no();
@@ -27,51 +36,76 @@ int main(){
     raiz->filhos[2] = novo_no();        //s
 
     //Descendentes de filhos[0]
-    // raiz->filhos[0]->filhos[1] = novo_no(); //er
-    //     raiz->filhos[0]->filhos[1]->filhos[1] = novo_no();  //err
-    //         raiz->filhos[0]->filhos[1]->filhos[1]->filhos[0] = novo_no();   //erre
-    //             raiz->filhos[0]->filhos[1]->filhos[1]->filhos[0]->filhos[2] = novo_no();    //erres
+    raiz->filhos[0]->filhos[1] = novo_no(); //er
+        raiz->filhos[0]->filhos[1]->filhos[1] = novo_no();  //err
+            raiz->filhos[0]->filhos[1]->filhos[1]->filhos[0] = novo_no();   //erre
+            raiz->filhos[0]->filhos[1]->filhos[1]->filhos[0]->info = TERMINAL;
+
+                raiz->filhos[0]->filhos[1]->filhos[1]->filhos[0]->filhos[2] = novo_no();    //erres
+                raiz->filhos[0]->filhos[1]->filhos[1]->filhos[0]->filhos[2]->info = TERMINAL;
 
 
-    // raiz->filhos[0]->filhos[2] = novo_no(); //es 
-    //     raiz->filhos[0]->filhos[2]->filhos[2] = novo_no();  //ess
-    //         raiz->filhos[0]->filhos[2]->filhos[2]->filhos[0] = novo_no();   //esse
-    //             raiz->filhos[0]->filhos[2]->filhos[2]->filhos[0]->filhos[2] = novo_no();    //esses
+    raiz->filhos[0]->filhos[2] = novo_no(); //es 
+    raiz->filhos[0]->filhos[2]->info = TERMINAL; 
 
-    // //Descendentes de filhos[1]
-    // raiz->filhos[1]->filhos[0] = novo_no(); //re
-    //     raiz->filhos[1]->filhos[0]->filhos[2] = novo_no();  //res
-    //         raiz->filhos[1]->filhos[0]->filhos[2]->filhos[0] = novo_no();   //rese
-    //             raiz->filhos[1]->filhos[0]->filhos[2]->filhos[0]->filhos[2] = novo_no();    //reses
+        raiz->filhos[0]->filhos[2]->filhos[2] = novo_no();  //ess
+            raiz->filhos[0]->filhos[2]->filhos[2]->filhos[0] = novo_no();   //esse
+            raiz->filhos[0]->filhos[2]->filhos[2]->filhos[0]->info = TERMINAL;
 
-    // //Descendentes de filhos[2]
-    // raiz->filhos[2]->filhos[0] = novo_no(); //se
-    //     raiz->filhos[2]->filhos[0]->filhos[1] = novo_no();  //ser
+                raiz->filhos[0]->filhos[2]->filhos[2]->filhos[0]->filhos[2] = novo_no();    //esses
+                raiz->filhos[0]->filhos[2]->filhos[2]->filhos[0]->filhos[2]->info = TERMINAL;
 
-    //         raiz->filhos[2]->filhos[0]->filhos[1]->filhos[0] = novo_no();   //sere
-    //             raiz->filhos[2]->filhos[0]->filhos[1]->filhos[0]->filhos[2] = novo_no();    //seres
 
-    //         raiz->filhos[2]->filhos[0]->filhos[1]->filhos[1] = novo_no();   //serr
-    //             raiz->filhos[2]->filhos[0]->filhos[1]->filhos[1]->filhos[0] = novo_no();    //serre
-    //                 raiz->filhos[2]->filhos[0]->filhos[1]->filhos[1]->filhos[0]->filhos[2] = novo_no(); //serres
+    //Descendentes de filhos[1]
+    raiz->filhos[1]->filhos[0] = novo_no(); //re
+    raiz->filhos[1]->filhos[0]->info = TERMINAL;
+
+        raiz->filhos[1]->filhos[0]->filhos[2] = novo_no();  //res
+        raiz->filhos[1]->filhos[0]->filhos[2]->info = TERMINAL;
+
+            raiz->filhos[1]->filhos[0]->filhos[2]->filhos[0] = novo_no();   //rese
+            raiz->filhos[1]->filhos[0]->filhos[2]->filhos[0]->info = TERMINAL;
+
+                raiz->filhos[1]->filhos[0]->filhos[2]->filhos[0]->filhos[2] = novo_no();    //reses
+                raiz->filhos[1]->filhos[0]->filhos[2]->filhos[0]->filhos[2]->info = TERMINAL;
+
+    //Descendentes de filhos[2]
+    raiz->filhos[2]->filhos[0] = novo_no(); //se
+    raiz->filhos[2]->filhos[0]->info = TERMINAL;
+
+        raiz->filhos[2]->filhos[0]->filhos[1] = novo_no();  //ser
+        raiz->filhos[2]->filhos[0]->filhos[1]->info = TERMINAL;
+
+            raiz->filhos[2]->filhos[0]->filhos[1]->filhos[0] = novo_no();   //sere
+                raiz->filhos[2]->filhos[0]->filhos[1]->filhos[0]->filhos[2] = novo_no();    //seres
+                raiz->filhos[2]->filhos[0]->filhos[1]->filhos[0]->filhos[2]->info = TERMINAL;
+
+            raiz->filhos[2]->filhos[0]->filhos[1]->filhos[1] = novo_no();   //serr
+                raiz->filhos[2]->filhos[0]->filhos[1]->filhos[1]->filhos[0] = novo_no();    //serre
+                raiz->filhos[2]->filhos[0]->filhos[1]->filhos[1]->filhos[0]->info = TERMINAL;
+
+                    raiz->filhos[2]->filhos[0]->filhos[1]->filhos[1]->filhos[0]->filhos[2] = novo_no(); //serres
+                    raiz->filhos[2]->filhos[0]->filhos[1]->filhos[1]->filhos[0]->filhos[2]->info = TERMINAL;
     
 
     /* Questão 3 */
-    raiz->filhos[0]->filhos[0] = novo_no();     //ee
-    raiz->filhos[0]->filhos[1] = novo_no();     //er
+    // raiz->filhos[0]->filhos[0] = novo_no();     //ee
+    // raiz->filhos[0]->filhos[1] = novo_no();     //er
 
-    raiz->filhos[1]->filhos[1] = novo_no();     //rr
+    // raiz->filhos[1]->filhos[1] = novo_no();     //rr
 
-    raiz->filhos[2]->filhos[1] = novo_no();     //sr
-    raiz->filhos[2]->filhos[2] = novo_no();     //ss
+    // raiz->filhos[2]->filhos[1] = novo_no();     //sr
+    // raiz->filhos[2]->filhos[2] = novo_no();     //ss
 
-    percorre(raiz, 0);
+    percorre(raiz);
 
     return 0;
 }
 
 No* novo_no(){
     No* pt = (No*) malloc(sizeof(No));
+    pt->info = NAO_TERMINAL;
+
     for(int i=0; i<M; i++){
         pt->filhos[i] = NULL;
     }
@@ -92,14 +126,34 @@ char de_posi_pra_digi(int p){
     return alfabeto[p];
 }
 
-void percorre(No* pt, int p){
+void percorre(No* pt){
     if(pt == NULL)
         return;
 
     for(int i = 0; i < M; i++){
         if(pt->filhos[i] != NULL){
             printf("%c ", alfabeto[i]); 
-            percorre(pt->filhos[i], i);
+            percorre(pt->filhos[i]);
         }
     }
+}
+
+No* busca_dig(char* x, No* pt, int l, int a){
+    No* w = pt;
+    int k = strlen(x);
+
+    if(pt == NULL) return NULL;
+
+    for(int i=l; i<k; i++){
+        a = 0;
+        int posi = de_digi_pra_posi(x[i]);
+
+        if(pt->filhos[posi] != NULL){
+            l++;
+            busca_dig(x, pt->filhos[posi], l, a);
+        }
+        return w;
+    }
+    a = 1;      //x conseguiu chegar até o último valor, então se encontra por completo na árvore
+    return w;
 }
